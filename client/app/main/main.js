f4bf7113ddc7b4104b1151c0701284d5d01cfa42
dsalme <dsalme@gmail.com>
2015-04-28 21:49:16 -0300

    first app boilerplate

---
+++ client/app/main/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('estudio39App')
+  .config(function ($stateProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/',
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
